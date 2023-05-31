28cf1886138d7e7a20387e1cb83c25ebbf74c5d7
Mariem3493 <127346106+Mariem3493@users.noreply.github.com>
2023-05-31 22:10:16 +0200

    Update App.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
+/*importation du css du composant parent*/
 import './App.css';
-import './Component/Profile/FullName';
+/*Importation du composant FullName*/
 import FullName from './Component/Profile/FullName';
+/*Importation du composant ProfilePhoto*/
 import ProfilePhoto from './Component/Profile/ProfilePhoto';
+/*Importation du css du boostrap*/
 import 'bootstrap/dist/css/bootstrap.min.css';
+/*Importation du composant Address*/
 import Address from './Component/Profile/Address';
+/*Fonction pour appel aux composants*/
 function App() {
+  /*Appel au composants FullName , Address, ProfilePhoto*/
   return (
     <div>
  <FullName/>
