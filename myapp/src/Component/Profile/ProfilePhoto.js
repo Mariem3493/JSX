0a56d628b2ce804b2cc8133111c4b57f59c4e77a
Mariem3493 <127346106+Mariem3493@users.noreply.github.com>
2023-06-03 14:32:05 +0200

    Update ProfilePhoto.js

--- myapp/src/Component/Profile/ProfilePhoto.js
+++ myapp/src/Component/Profile/ProfilePhoto.js
@@ -1,6 +1,10 @@
+/*importation de la bibliothèque react*/
 import React from 'react'
+/*importation de l image i1*/
 import i1 from '../Profile/Assets/1.jpg';
+/*importation du fichier css de l image*/
 import '../Image.css';
+/*iFonction ProfilePhoto*/
 const ProfilePhoto = () => {
   return (
     <div>
