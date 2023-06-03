3b04ffb056b95a931851d21681f98199c6056481
Mariem3493 <127346106+Mariem3493@users.noreply.github.com>
2023-06-03 14:27:53 +0200

    Update App.test.js

--- myapp/src/App.test.js
+++ myapp/src/App.test.js
@@ -1,6 +1,8 @@
+/*importation des éléments render et screen de la bibliothèque testing-library react*/
 import { render, screen } from '@testing-library/react';
+/*importation du composant App*/
 import App from './App';
-
+/*Fonction test de vérification si le rendu de la fonction App fonctionne correctement*/
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
